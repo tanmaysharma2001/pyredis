5cc6c0839711b24036c2b65b887676fc3057b7da
Tanmay Sharma <sharmatanmay617@gmail.com>
2024-05-22 02:47:23 +0300

    Replication Finished

--- app/main.py
+++ app/main.py
@@ -1,182 +1,279 @@
+import asyncio
 import base64
 import socket
 import threading
+from threading import Thread
 import time
-
-# Arguement Parser
+import re
 import argparse
+
+# Lock for thread safety
+lock = threading.Lock()
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", help="Port Number")
-parser.add_argument("--replicaof", nargs=2, help="Replicate to another redis instance")
+parser.add_argument("-p", "--port", help="Port Number", type=int)
+parser.add_argument("--replicaof", nargs=1, help="Replicate to another redis instance")
 
 # In-memory key-value store
 key_value_store = {}
-is_replica = False
 master_replid = '8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb'
 master_repl_offset = 0
 
-# Empty RDB file content (base64 representation)
-EMPTY_RDB_BASE64 = b"UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
+# Master HOST and PORT
+master_host = ''
+master_port = ''
+replicas = []
 
-# Parser function for Redis protocol
-def parse_redis_protocol(data):
-    lines = data.split(b'\r\n')
-    command = []
-    i = 0
-    while i < len(lines):
-        line = lines[i]
-        if line.startswith(b'*'):
-            # The number of arguments
-            pass
-        elif line.startswith(b'$'):
-            # The length of the next argument
-            length = int(line[1:])
-            i += 1  # Move to the argument
-            argument = lines[i][:length]  # Get the argument with the specified length
-            command.append(argument.decode())  # Assuming UTF-8 encoding
-        i += 1
-    return command
+# MASTER SOCKET for Replica
+master_socket = None
+role = "master"
+is_replica = False
+replica_offset = 0
+
+# Empty RDB file content (base64 representation)
+EMPTY_RDB_FILE = b"UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
 
 
 def handle_set_command(parsed_command):
+    global is_replica, master_host, master_port
     key, value = parsed_command[1], parsed_command[2]
     expiry = None
     if "px" in parsed_command:
         px_index = parsed_command.index("px")
         if px_index + 1 < len(parsed_command):
-            expiry = int(time.time() * 1000) + int(parsed_command[px_index + 1])  # Convert to milliseconds and add to current time
-    key_value_store[key] = (value, expiry)
+            expiry = int(time.time() * 1000) + int(
+                parsed_command[px_index + 1])  # Convert to milliseconds and add to current time
+    with lock:
+        key_value_store[key] = (value, expiry)
 
 
-def handle_get_command(clientsocket, parsed_command):
+def handle_get_command(parsed_command):
     key = parsed_command[1]
-    value, expiry = key_value_store.get(key, (None, None))
+    with lock:
+        value, expiry = key_value_store.get(key, (None, None))
     if value is not None:
         # Check if the key has expired
         if expiry is not None and int(time.time() * 1000) > expiry:
-            del key_value_store[key]  # Remove expired key
-            clientsocket.sendall(b"$-1\r\n")
+            with lock:
+                del key_value_store[key]  # Remove expired key
+            return b"$-1\r\n"
         else:
             response = f"${len(value)}\r\n{value}\r\n"
-            clientsocket.sendall(response.encode())
+            return response.encode()
     else:
-        clientsocket.sendall(b"$-1\r\n")
+        return b"$-1\r\n"
 
 
-def handle_info_command(clientsocket, parsed_command):
+def handle_info_command(parsed_command):
     global is_replica, master_replid, master_repl_offset
     if parsed_command[1] == 'replication':
         if is_replica:
-            clientsocket.sendall(b"$10\r\nrole:slave\r\n")
+            return b"$10\r\nrole:slave\r\n"
         else:
-            clientsocket.sendall(f"${2 + 11 + len(master_replid) + len('master_replid:') + len(str(master_repl_offset)) + len('master_repl_offset:')}\r\nrole:master,master_replid:{master_replid},master_repl_offset:{master_repl_offset}\r\n".encode())
-
-def on_new_client(clientsocket, addr):
-    print(f"Connected by {addr}")
-    while True:
-        data = clientsocket.recv(1024)       
-        if not data:
-            break
-
-        # Parsing the Data according to the Redis Protocol
-        parsed_command = parse_redis_protocol(data)
-
-        if not parsed_command:
-            continue
-
-        # Handling the set command
-        # Handling SET command with optional PX for expiry
-        if parsed_command[0].lower() == 'set':
-            handle_set_command(parsed_command)
-            clientsocket.sendall(b"+OK\r\n")
-        
-        # Handling GET command
-        elif parsed_command[0].lower() == 'get' and len(parsed_command) == 2:
-            handle_get_command(clientsocket, parsed_command)
-        
-        # Handling the INFO command
-        elif parsed_command[0].lower() == 'info' and len(parsed_command) == 2:
-            handle_info_command(clientsocket, parsed_command)
-        
-        # Handling the echo command
-        elif parsed_command[0] == 'ECHO' and len(parsed_command) > 1:
-            response_message = f"+{parsed_command[1]}\r\n"
-            clientsocket.sendall(response_message.encode())
-        
-        else:
-            clientsocket.sendall(b"+PONG\r\n")
-    clientsocket.close()
+            return f"${2 + 11 + len(master_replid) + len('master_replid:') + len(str(master_repl_offset)) + len('master_repl_offset:')}\r\nrole:master,master_replid:{master_replid},master_repl_offset:{master_repl_offset}\r\n".encode()
 
 
-def replicate_to_master(master_host, master_port, portNumber):
+def propogate_to_slave(replica_socket, message):
+    replica_socket.write(message)
+    replica_socket.drain()
+
+
+def handle_command(parsed_command, writer=None):
     global is_replica
+    responses = []
+
+    if parsed_command[0] == 'set':
+        handle_set_command(parsed_command)
+        if not is_replica:
+            responses.append(b"+OK\r\n")
+            with lock:
+                for replica_socket in replicas:
+                    propogate_to_slave(replica_socket,
+                                       f"*3\r\n$3\r\nSET\r\n${len(parsed_command[1])}\r\n{parsed_command[1]}\r\n${len(parsed_command[2])}\r\n{parsed_command[2]}\r\n".encode())
+
+    # Handling GET command
+    elif parsed_command[0] == 'get' and len(parsed_command) == 2:
+        responses.append(handle_get_command(parsed_command))
+
+    # Handling the INFO command
+    elif parsed_command[0] == 'info' and len(parsed_command) == 2:
+        responses.append(handle_info_command(parsed_command))
+
+    elif parsed_command[0] == 'wait' and len(parsed_command) > 1:
+        responses.append(f":{len(replicas)}\r\n".encode())
+
+    # Handling the echo command
+    elif parsed_command[0] == 'echo' and len(parsed_command) > 1:
+        response_message = f"+{parsed_command[1]}\r\n".encode()
+        # writer.write(response_message.encode())
+        responses.append(response_message)
+
+    elif parsed_command[0] == 'replconf' and len(parsed_command) > 1:
+        responses.append(b'+OK\r\n')
+
+    elif parsed_command[0] == 'psync' and parsed_command[1] == '?' and parsed_command[2] == '-1':
+        # Send FULLRESYNC response
+        response = f"+FULLRESYNC {master_replid} 0\r\n".encode()
+        responses.append(response)
+        if not is_replica:
+            with lock:
+                replicas.append(writer)
+        # Send empty RDB file
+        rdb_content = base64.b64decode(EMPTY_RDB_FILE)
+        rdb_length = len(rdb_content)
+        responses.append(f"${rdb_length}\r\n".encode() + rdb_content)
+    else:
+        responses.append(b"+PONG\r\n")
+
+    return responses
+
+
+# Parser function for Redis protocol
+def parse_redis_protocol(data):
+    # data = data.decode("utf-8")
+    command_list = data.split("\r\n")
+    if command_list[-1] == "":
+        command_list.pop()
+    first_arg = command_list.pop(0)
+    arg_num_list = re.findall(pattern="\*\d", string=first_arg)
+    if len(arg_num_list) != 1:
+        return "-Wrong command1\r\n"
+    arg_num = int(arg_num_list[0].replace("*", ""))
+    if arg_num not in range(1, 6):
+        return "-Wrong command2\r\n"
+    command_list = list(
+        filter(
+            lambda x: True if "$" not in x else False,
+            command_list,
+        )
+    )
+    command_list[0] = command_list[0].lower()
+    return command_list
+
+
+async def on_new_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    global is_replica, replicas
+
+    try:
+        print(f"Reader: {reader}")
+
+        while True:
+            data = await reader.read(4096)
+            if not data:
+                print(f"No data received from {reader}")
+                break
+
+            data = data.decode('utf-8')
+
+            # Parsing the Data according to the Redis Protocol
+            parsed_commands = parse_redis_protocol(data)
+
+            if not parsed_commands:
+                continue
+
+            responses = handle_command(parsed_commands, writer)
+            for response in responses:
+                writer.write(response)
+
+        await writer.drain()
+
+    except Exception as err:
+        print(f"Connection closed {err}")
+
+
+
+def get_from_master(master_socket: socket.socket):
+    global replica_offset
     while True:
         try:
-            master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            master_socket.connect((master_host, master_port))
+            # print("next master ")
+            data = master_socket.recv(4039)  # .decode("utf-8")
+            if data:
+                print(data)
+                pattern = r'\*[\d]+\r\n(?:\$[\d]+\r\n[^\r\n]+\r\n)+'
+                command_list = re.findall(pattern, data.decode('utf-8'))
+                print(command_list)
+                for command in command_list:
+                    if command:
+                        # if command.startswith(b'+FULLRESYNC') or command.startswith(b'*\r\n'):
+                        #     continue
+                        parsed_commands = parse_redis_protocol(command)
+                        if parsed_commands[0] == 'replconf' and parsed_commands[1] == "GETACK":
+                            master_socket.sendall(
+                                f"*3\r\n${len('replconf')}\r\nREPLCONF\r\n${len('ack')}\r\nACK\r\n${len(str(replica_offset))}\r\n{replica_offset}\r\n".encode())
+                        else:
+                            handle_command(parsed_commands)
+
+                        replica_offset = replica_offset + len(command)
+
+        except Exception as err:
+            print(f"Connection with master {master_socket}: {err}")
+
+
+async def create_server(master_host, master_port, port_number):
+    global role, is_replica, master_socket
+    if role == "slave":
+        try:
+            slave_socket = socket.socket()
+            slave_socket.connect((master_host, master_port))
             is_replica = True
 
             # Part 1 of the handshake
-            master_socket.sendall(b"*1\r\n$4\r\nPING\r\n")
+            slave_socket.sendall(b"*1\r\n$4\r\nPING\r\n")
             # Wait for response to PING
-            data = master_socket.recv(1024)
+            data = slave_socket.recv(1024)
             if data != b"+PONG\r\n":
                 print("Failed to receive response to PING")
                 return
 
             # Part 2 of the handshake
-            master_socket.sendall(b"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n")
-            master_socket.sendall(b"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+            slave_socket.sendall(f"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{port_number}\r\n".encode())
+            slave_socket.sendall(b"*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
 
             # Wait for response to REPLCONF
             for _ in range(2):
-                data = master_socket.recv(1024)
+                data = slave_socket.recv(1024)
                 if data != b"+OK\r\n":
                     print("Failed to receive response to REPLCONF")
                     return
-            
+
             # Third Part of the handshake
             # Sending PSYNC
-            master_socket.sendall(b"*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
-            
-            # data = master_socket.recv(1024)
-            # if data != b"+PONG\r\n":
-            #     print("Failed to receive response to PING")
-            #     return
-            # Send empty RDB file
-            rdb_content = base64.b64decode(EMPTY_RDB_BASE64)
-            rdb_length = len(rdb_content)
-            master_socket.sendall(b"$" + str(rdb_length).encode() + b"\r\n" + rdb_content)
+            slave_socket.sendall(b"*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
+            for _ in range(2):
+                data = slave_socket.recv(2048)
 
+            # Put receiving the empty file from master to different thread
+            Thread(target=get_from_master, args=(slave_socket,)).start()
 
         except Exception as e:
             print("Error in replication:", e)
+    server = await asyncio.start_server(on_new_client, "localhost", port_number)
+    async with server:
+        await server.serve_forever()
+
 
 def main():
+    global role, master_host, master_port
     print("Logs from your program will appear here!")
 
-    portNumber = 6379
+    port_number = 6379
     args = parser.parse_args()
     if args.port:
-        portNumber = int(args.port)
+        port_number = args.port
+
+    role = "master"
 
     if args.replicaof:
-        master_host, master_port = args.replicaof
-        master_port = int(master_port)
-        threading.Thread(target=replicate_to_master, args=(master_host, master_port, portNumber)).start()
+        master_address = args.replicaof
+        master_address = master_address[0].split(" ")
+        master_host = master_address[0]
+        master_port = int(master_address[1])
+        role = "slave"
 
+    print(f"Create redis server as {role} on port {port_number}")
+    asyncio.run(create_server(master_host, master_port, port_number))
 
-    try:
-        server_socket = socket.create_server(("localhost", portNumber), reuse_port=True)
-        
-        while True:
-            conn, addr = server_socket.accept() # wait for client
-            client_thread = threading.Thread(target=on_new_client, args=(conn, addr))
-            client_thread.start()
-    except:
-        print("Except an error occured")
-    
-    server_socket.close()
 
 if __name__ == "__main__":
     main()
